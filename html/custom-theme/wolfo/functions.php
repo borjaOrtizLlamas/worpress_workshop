c9b639ac68ab9cee47f526b15bf63a9ee55e4472
unknown <ortizllamasborja@gmail.com>
2021-10-22 17:11:58 +0200

    deleting theme

--- html/custom-theme/wolfo/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @copyright Copyright (c) 2021 WolfCoding (https://wolfcoding.com). All rights reserved.
- */
-if (!defined('ABSPATH')) {
-	return;
-}
-
-require_once get_template_directory() . '/inc/init.php';
--- html/custom-theme/wolfo/inc/core/style.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @copyright Copyright (c) 2021 WolfCoding (https://wolfcoding.com). All rights reserved.
- */
-if (!defined('ABSPATH')) {
-    return;
-}
-
-
